f69c1472df720e041169a46e394b945ae30e4b71
gsiqueiros <adrianslopez12@gmail.com>
2022-06-09 08:13:57 -0700

    Add

--- Almacen1/Class/ClsGlobales.cs
+++ Almacen1/Class/ClsGlobales.cs
@@ -6,9 +6,7 @@ namespace Almacen1.Class
     {
         public static MySqlConnection GetConnection()
         {
-            //return new MySqlConnection("server=127.0.0.1;PORT=3306;database=db_almacen;Uid=root;pwd=;");
             return new MySqlConnection("server=189.204.133.38;PORT=3306;database=db_almacen;Uid=mlegalmacenburgos;pwd=12mL3GN4jane;");
-
         }
     }
 }
